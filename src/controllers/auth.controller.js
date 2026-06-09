54c8741b5baa3e05d0b0aaa69b30109ad5787c30
Rajtiwari0202 <rajtiwari16916@gmail.com>
2026-06-09 22:42:05 +0530

    Completed auth and music routes and Middlewares

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,54 +2,104 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+
 async function registerUser(req, res) {
-  try {
-    const { userName, email, password, role = "user" } = req.body;
+
+    const { username, email, password, role = "user" } = req.body;
 
     const isUserAlreadyExists = await userModel.findOne({
-      $or: [{ userName }, { email }],
-    });
+        $or: [
+            { username },
+            { email }
+        ]
+    })
 
     if (isUserAlreadyExists) {
-      return res.status(409).json({
-        message: "User already exists",
-      });
+        return res.status(409).json({ message: "User already exists" })
     }
 
-    const hash = await bcrypt.hash(password, 10);
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
-      userName,
-      email,
-      password: hash,
-      role,
-    });
+        username,
+        email,
+        password: hash,
+        role
+    })
 
-    const token = jwt.sign(
-      {
+    const token = jwt.sign({
         id: user._id,
         role: user.role,
-      },
-      process.env.JWT_SECRET
-    );
+    }, process.env.JWT_SECRET)
 
-    res.cookie("token", token);
+
+    res.cookie("token", token)
+
 
     res.status(201).json({
-      message: "User registered successfully",
-      user: {
-        id: user._id,
-        userName: user.userName,
-        email: user.email,
-        role: user.role,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+        }
+    })
+
 }
 
-module.exports = { registerUser };
+
+async function loginUser(req, res) {
+
+    const { username, email, password } = req.body;
+
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" })
+    }
+
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+        }
+    })
+
+
+
+
+}
+
+async function logoutUser(req, res) {
+    res.clearCookie("token")
+    res.status(200).json({ message: "User logged out successfully" })
+}
+
+
+module.exports = { registerUser, loginUser, logoutUser }
--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database connected successfully");
+
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Database connected successfully');
+
     }
-    catch(error){
-        console.error("Database connection error: ",error);
+    catch (error) {
+        console.error('Database connection error:', error);
     }
+
 }
 
 module.exports = connectDB;
